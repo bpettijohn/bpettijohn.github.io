69bd37155395eb7ed02a8f13b051bd0ab60cb00d
bpettijohn <bpettijohn@users.noreply.github.com>
2024-03-25 18:46:15 +0000

    deploy: 2192ded648ff0c94c342926273903f10e5494f93

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,24 @@ module.exports = {
     './*.html',
   ],
   theme: {
-    theme: {
-      extend: {},
+    extend: {
+      backgroundImage: {
+        'leaves': "url('/assets/images/leaves.jpg')",
+      },
+      colors: {
+        'grey': '#231f20',
+        'pink': '#ef60a2',
+        'purple': '#6a4f9e',
+        'white': '#f8cde0'
+      }
     },
+    backgroundSize: {
+      'auto': 'auto',
+      'cover': 'cover',
+      'contain': 'contain',
+      '16': '50rem',
+    },
+
   },
   plugins: []
 }
