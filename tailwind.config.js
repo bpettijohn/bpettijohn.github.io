5f30819b93f28caab1ffea4684b6001e584a1360
Ben Pettijohn <bpettijohn@meteorcomm.com>
2024-04-02 10:11:27 -0700

    switching to vitepress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './_drafts/**/*.html',
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.md',
-    './*.html',
-  ],
+  content: ["./docs/.vitepress/**/*.{html,js,ts,vue}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'leaves': "url('/assets/images/leaves.jpg')",
-      },
-      colors: {
-        'grey': '#231f20',
-        'pink': '#ef60a2',
-        'purple': '#6a4f9e',
-        'white': '#f8cde0'
-      }
-    },
-    backgroundSize: {
-      'auto': 'auto',
-      'cover': 'cover',
-      'contain': 'contain',
-      '16': '50rem',
-    },
-
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
+
